e63cdf384606a56d2ce6ea80f0effe2ceb0235c6
tomericco <nrJtsMy7aCVHsqzi2az+7H96h4l/go/OxA4fXSfXHW0=>
2024-10-31 18:01:48 +0200

    Initial Commit

---
+++ frontitude-demo-app-ios/frontitude_demo_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontitude_demo_app_iosApp.swift
+//  frontitude-demo-app-ios
+//
+//  Created by Tomer Gabbai on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct frontitude_demo_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontitude-demo-app-iosTests/frontitude_demo_app_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  frontitude_demo_app_iosTests.swift
+//  frontitude-demo-app-iosTests
+//
+//  Created by Tomer Gabbai on 31/10/2024.
+//
+
+import Testing
+@testable import frontitude_demo_app_ios
+
+struct frontitude_demo_app_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
